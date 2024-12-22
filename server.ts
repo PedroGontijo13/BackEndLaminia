c69bfb4d3ed14a70a0bbdb9776e1d9a758d942aa
Pedro Gontijo <pedrotolentino2002@gmail.com>
2024-12-22 16:00:56 -0300

    `Added MongoDB and Mongoose dependencies, updated server.ts to connect to MongoDB and changed port number.`

--- server.ts
+++ server.ts
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const mongoURI = 'mongodb://localhost:27017/local';
+
+const mongodb = mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Conectado ao MongoDB com sucesso!'))
+    .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello, Express.js Server!</h1>');
 });
 
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
