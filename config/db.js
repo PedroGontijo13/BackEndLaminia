724084e9a780276614600301352c58df1b95ecd8
Pedro Gontijo <pedrotolentino2002@gmail.com>
2024-12-22 23:17:19 -0300

    `Update .gitignore to include .env and remove node_modules, update MongoDB connection URI in config/db.js to use environment variable`

--- config/db.js
+++ config/db.js
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose';
+
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/local');
+        const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/local';
+
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
         console.log('Conectado ao MongoDB com sucesso!');
-    }
-    catch (err) {
+    } catch (err) {
         console.error('Erro ao conectar ao MongoDB:', err);
         process.exit(1);
     }
